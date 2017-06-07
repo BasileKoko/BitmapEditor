8be00e46b50766ff82d48280a4ffe4a5626f90f1
Basile Koko <basile_koko@hotmail.com>
2017-06-07 21:43:42 +0100

    adding test for parsing provided file

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -1,6 +1,9 @@
 require 'bitmap_editor'
 
 describe BitmapEditor do
+  before do
+    @file = 'examples/show.txt'
+  end
 
   describe '#get_file' do
     it "returns message to provide correct file when no file is provided" do
@@ -11,6 +14,12 @@ describe BitmapEditor do
     it "returns message to provide correct file when file provided does not exist" do
       expect(STDOUT).to receive(:puts).with("Please provide correct file")
       subject.get_file('nofile.txt')
+    end
+  end
+
+  describe '#parse_file' do
+    it 'returns content of the file' do
+      expect(subject.parse_file(@file)[0]).to eq 'I 5 6'
     end
   end
 end
