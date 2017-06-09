656730a459a0cd4b89d48bc8a60c0ecf479098de
Basile Koko <basile_koko@hotmail.com>
2017-06-09 20:47:09 +0100

    adding test: it creates new image with columns and rows

--- spec/image_spec.rb
+++ spec/image_spec.rb
@@ -1,5 +1,16 @@
 require 'image'
 
 describe Image do
+  before do
+    @image = Image.new(5,6)
+  end
+  
+  describe '#initialize' do
+    it 'creates new image with columns and rows' do
+      allow(Image).to receive(:new).with(5,6)
+      expect(@image.columns).to eq 5
+      expect(@image.rows).to eq 6
+    end
+  end
 
 end
