6a1ce064c475f03bb56956a2601f13d23ad55ddb
Basile Koko <basile_koko@hotmail.com>
2017-06-12 12:16:58 +0100

    adding colour.upcase to method colour_pixel to handle colour in lowercase

--- lib/image.rb
+++ lib/image.rb
@@ -7,7 +7,7 @@ class Image
   end
 
   def colour_pixel(x, y, colour)
-    @pixels[y - 1][x - 1] = colour
+    @pixels[y - 1][x - 1] = colour.upcase
   end
 
   def draw_vertical_segment(column, start_row, end_row, colour)
